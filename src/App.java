c913baebcb1d096fe9eb5f9514b35f7488922251
JoaoPedroDaCunha <joaopedrodacunha.santos@gmail.com>
2024-10-08 11:13:12 -0300

    basic class

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String test =sc.nextLine().trim().toLowerCase().replace(" ","");
+        System.out.println(test);
+        sc.close();
+    }
+}
---
+++ src/Program/App.java
@@ -0,0 +1,8 @@
+package Program;
+
+public class App {
+    public static void main(String[] args) {
+        
+    }
+    
+}
